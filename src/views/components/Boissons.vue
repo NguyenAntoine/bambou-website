d6e40773dd60d3f9e047d5715b128dc29e439bac
Antoine <antoine.nguyen3@epsi.fr>
2018-11-08 15:19:18 +0100

    Ajout de boissons

--- src/views/components/Boissons.vue
+++ src/views/components/Boissons.vue
@@ -4,11 +4,11 @@
       <div class="md-layout-item md-medium-size-33 md-small-size-100">
         <md-card class="md-card-plain">
           <div class="md-layout-item md-size-50 mx-auto">
-            <img :src="rouleau" alt="Thumbnail Image" class="img-raised rounded-circle img-fluid">
+            <img :src="coca" alt="Thumbnail Image" class="img-raised rounded-circle img-fluid">
           </div>
-          <h4 class="card-title">Rouleau de printemps
+          <h4 class="card-title">Coca Cola
             <br>
-            <small class="card-description text-muted">Porc et crevette</small>
+            <small class="card-description text-muted">33cl / 1.75L</small>
           </h4>
 
           <md-card-content>
@@ -19,11 +19,11 @@
       <div class="md-layout-item md-medium-size-33 md-small-size-100">
         <md-card class="md-card-plain">
           <div class="md-layout-item md-size-50 mx-auto">
-            <img :src="salade" alt="Thumbnail Image" class="img-raised rounded-circle img-fluid">
+            <img :src="cocaZero" alt="Thumbnail Image" class="img-raised rounded-circle img-fluid">
           </div>
-          <h4 class="card-title">Salade vietnamienne
+          <h4 class="card-title">Coca Cola Zero
             <br>
-            <small class="card-description text-muted">Vegan</small>
+            <small class="card-description text-muted">33cl</small>
           </h4>
 
           <md-card-content>
@@ -31,7 +31,115 @@
           </md-card-content>
         </md-card>
       </div>
-      <div class="md-layout-item md-medium-size-33 md-small-size-100"></div>
+      <div class="md-layout-item md-medium-size-33 md-small-size-100">
+        <md-card class="md-card-plain">
+          <div class="md-layout-item md-size-50 mx-auto">
+            <img :src="orangina" alt="Thumbnail Image" class="img-raised rounded-circle img-fluid">
+          </div>
+          <h4 class="card-title">Orangina
+            <br>
+            <small class="card-description text-muted">33cl</small>
+          </h4>
+
+          <md-card-content>
+            <p class="card-description">You can write here details about one of your team members. You can give more details about what they do. Feel free to add some <a href="#">links</a> for people to be able to follow them outside the site.</p>
+          </md-card-content>
+        </md-card>
+      </div>
+    </div>
+    <div class="md-layout">
+      <div class="md-layout-item md-medium-size-33 md-small-size-100">
+        <md-card class="md-card-plain">
+          <div class="md-layout-item md-size-50 mx-auto">
+            <img :src="icetea" alt="Thumbnail Image" class="img-raised rounded-circle img-fluid">
+          </div>
+          <h4 class="card-title">Ice Tea
+            <br>
+            <small class="card-description text-muted">33cl / 1.5L</small>
+          </h4>
+
+          <md-card-content>
+            <p class="card-description">You can write here details about one of your team members. You can give more details about what they do. Feel free to add some <a href="#">links</a> for people to be able to follow them outside the site.</p>
+          </md-card-content>
+        </md-card>
+      </div>
+      <div class="md-layout-item md-medium-size-33 md-small-size-100">
+        <md-card class="md-card-plain">
+          <div class="md-layout-item md-size-50 mx-auto">
+            <img :src="litchi" alt="Thumbnail Image" class="img-raised rounded-circle img-fluid">
+          </div>
+          <h4 class="card-title">Boissons au Litchi
+            <br>
+            <small class="card-description text-muted">33cl</small>
+          </h4>
+
+          <md-card-content>
+            <p class="card-description">You can write here details about one of your team members. You can give more details about what they do. Feel free to add some <a href="#">links</a> for people to be able to follow them outside the site.</p>
+          </md-card-content>
+        </md-card>
+      </div>
+      <div class="md-layout-item md-medium-size-33 md-small-size-100">
+        <md-card class="md-card-plain">
+          <div class="md-layout-item md-size-50 mx-auto">
+            <img :src="fruitsPassion" alt="Thumbnail Image" class="img-raised rounded-circle img-fluid">
+          </div>
+          <h4 class="card-title">Boisson au fruits de la passion
+            <br>
+            <small class="card-description text-muted">33cl</small>
+          </h4>
+
+          <md-card-content>
+            <p class="card-description">You can write here details about one of your team members. You can give more details about what they do. Feel free to add some <a href="#">links</a> for people to be able to follow them outside the site.</p>
+          </md-card-content>
+        </md-card>
+      </div>
+    </div>
+    <div class="md-layout">
+      <div class="md-layout-item md-medium-size-33 md-small-size-100">
+        <md-card class="md-card-plain">
+          <div class="md-layout-item md-size-50 mx-auto">
+            <img :src="biereTsingtao" alt="Thumbnail Image" class="img-raised rounded-circle img-fluid">
+          </div>
+          <h4 class="card-title">Bière Tsingtao
+            <br>
+            <small class="card-description text-muted">0.5L</small>
+          </h4>
+
+          <md-card-content>
+            <p class="card-description">You can write here details about one of your team members. You can give more details about what they do. Feel free to add some <a href="#">links</a> for people to be able to follow them outside the site.</p>
+          </md-card-content>
+        </md-card>
+      </div>
+      <div class="md-layout-item md-medium-size-33 md-small-size-100">
+        <md-card class="md-card-plain">
+          <div class="md-layout-item md-size-50 mx-auto">
+            <img :src="vinRouge" alt="Thumbnail Image" class="img-raised rounded-circle img-fluid">
+          </div>
+          <h4 class="card-title">Vin rouge
+            <br>
+            <small class="card-description text-muted">1.5L</small>
+          </h4>
+
+          <md-card-content>
+            <p class="card-description">You can write here details about one of your team members. You can give more details about what they do. Feel free to add some <a href="#">links</a> for people to be able to follow them outside the site.</p>
+          </md-card-content>
+        </md-card>
+      </div>
+      <div class="md-layout-item md-medium-size-33 md-small-size-100">
+        <md-card class="md-card-plain">
+          <div class="md-layout-item md-size-50 mx-auto">
+            <img :src="vinRose" alt="Thumbnail Image" class="img-raised rounded-circle img-fluid">
+          </div>
+          <h4 class="card-title">Vin rosé
+            <br>
+            <small class="card-description text-muted">1.5L</small>
+          </h4>
+
+          <md-card-content>
+            <p class="card-description">You can write here details about one of your team members. You can give more details about what they do. Feel free to add some <a href="#">links</a> for people to be able to follow them outside the site.</p>
+          </md-card-content>
+        </md-card>
+      </div>
     </div>
   </div>
 </template>
@@ -41,8 +149,15 @@ export default {
   name: "Boissons",
   data() {
     return {
-      rouleau: require("@/assets/img/examples/studio-1.jpg"),
-      salade: require("@/assets/img/examples/studio-1.jpg")
+      coca: require("@/assets/img/examples/studio-1.jpg"),
+      cocaZero: require("@/assets/img/examples/studio-1.jpg"),
+      icetea: require("@/assets/img/examples/studio-1.jpg"),
+      orangina: require("@/assets/img/examples/studio-1.jpg"),
+      litchi: require("@/assets/img/examples/studio-1.jpg"),
+      fruitsPassion: require("@/assets/img/examples/studio-1.jpg"),
+      biereTsingtao: require("@/assets/img/examples/studio-1.jpg"),
+      vinRouge: require("@/assets/img/examples/studio-1.jpg"),
+      vinRose: require("@/assets/img/examples/studio-1.jpg")
     };
   }
 };
