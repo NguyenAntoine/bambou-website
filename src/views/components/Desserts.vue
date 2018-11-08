da58e15f8c85890bb207b9418e279d42d6429951
Antoine <antoine.nguyen3@epsi.fr>
2018-11-08 15:02:33 +0100

    Ajout des desserts

--- src/views/components/Desserts.vue
+++ src/views/components/Desserts.vue
@@ -4,12 +4,9 @@
       <div class="md-layout-item md-medium-size-33 md-small-size-100">
         <md-card class="md-card-plain">
           <div class="md-layout-item md-size-50 mx-auto">
-            <img :src="rouleau" alt="Thumbnail Image" class="img-raised rounded-circle img-fluid">
+            <img :src="biscuitSesame" alt="Thumbnail Image" class="img-raised rounded-circle img-fluid">
           </div>
-          <h4 class="card-title">Rouleau de printemps
-            <br>
-            <small class="card-description text-muted">Porc et crevette</small>
-          </h4>
+          <h4 class="card-title">Biscuits aux sésames</h4>
 
           <md-card-content>
             <p class="card-description">You can write here details about one of your team members. You can give more details about what they do. Feel free to add some <a href="#">links</a> for people to be able to follow them outside the site.</p>
@@ -19,18 +16,42 @@
       <div class="md-layout-item md-medium-size-33 md-small-size-100">
         <md-card class="md-card-plain">
           <div class="md-layout-item md-size-50 mx-auto">
-            <img :src="salade" alt="Thumbnail Image" class="img-raised rounded-circle img-fluid">
+            <img :src="nougat" alt="Thumbnail Image" class="img-raised rounded-circle img-fluid">
           </div>
-          <h4 class="card-title">Salade vietnamienne
-            <br>
-            <small class="card-description text-muted">Vegan</small>
-          </h4>
+          <h4 class="card-title">Nougat aux sésames</h4>
 
           <md-card-content>
             <p class="card-description">You can write here details about one of your team members. You can give more details about what they do. Feel free to add some <a href="#">links</a> for people to be able to follow them outside the site.</p>
           </md-card-content>
         </md-card>
       </div>
+      <div class="md-layout-item md-medium-size-33 md-small-size-100">
+        <md-card class="md-card-plain">
+          <div class="md-layout-item md-size-50 mx-auto">
+            <img :src="gateauChinois" alt="Thumbnail Image" class="img-raised rounded-circle img-fluid">
+          </div>
+          <h4 class="card-title">Gâteau chinois soja-coco</h4>
+
+          <md-card-content>
+            <p class="card-description">You can write here details about one of your team members. You can give more details about what they do. Feel free to add some <a href="#">links</a> for people to be able to follow them outside the site.</p>
+          </md-card-content>
+        </md-card>
+      </div>
+    </div>
+    <div class="md-layout">
+      <div class="md-layout-item md-medium-size-33 md-small-size-100">
+        <md-card class="md-card-plain">
+          <div class="md-layout-item md-size-50 mx-auto">
+            <img :src="boulesCoco" alt="Thumbnail Image" class="img-raised rounded-circle img-fluid">
+          </div>
+          <h4 class="card-title">Boules coco</h4>
+
+          <md-card-content>
+            <p class="card-description">You can write here details about one of your team members. You can give more details about what they do. Feel free to add some <a href="#">links</a> for people to be able to follow them outside the site.</p>
+          </md-card-content>
+        </md-card>
+      </div>
+      <div class="md-layout-item md-medium-size-33 md-small-size-100"></div>
       <div class="md-layout-item md-medium-size-33 md-small-size-100"></div>
     </div>
   </div>
@@ -41,13 +62,11 @@ export default {
   name: "Desserts",
   data() {
     return {
-      rouleau: require("@/assets/img/examples/studio-1.jpg"),
-      salade: require("@/assets/img/examples/studio-1.jpg")
-    }
+      biscuitSesame: require("@/assets/img/examples/studio-1.jpg"),
+      nougat: require("@/assets/img/examples/studio-1.jpg"),
+      gateauChinois: require("@/assets/img/examples/studio-1.jpg"),
+      boulesCoco: require("@/assets/img/examples/studio-1.jpg")
+    };
   }
-}
+};
 </script>
-
-<style scoped>
-
-</style>
