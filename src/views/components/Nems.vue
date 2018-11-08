78eccc637181731a4c27f4bd4c752993b9285740
Antoine <antoine.nguyen3@epsi.fr>
2018-11-08 13:30:41 +0100

    Changement de plats de section

--- src/views/components/Nems.vue
+++ src/views/components/Nems.vue
@@ -63,21 +63,7 @@
           </md-card-content>
         </md-card>
       </div>
-      <div class="md-layout-item md-medium-size-33 md-small-size-100">
-        <md-card class="md-card-plain">
-          <div class="md-layout-item md-size-50 mx-auto">
-            <img :src="brochettesPoulet" alt="Thumbnail Image" class="img-raised rounded-circle img-fluid">
-          </div>
-          <h4 class="card-title">Brochettes de poulet
-            <br>
-            <small class="card-description text-muted">Poulet</small>
-          </h4>
-
-          <md-card-content>
-            <p class="card-description">You can write here details about one of your team members. You can give more details about what they do. Feel free to add some <a href="#">links</a> for people to be able to follow them outside the site.</p>
-          </md-card-content>
-        </md-card>
-      </div>
+      <div class="md-layout-item md-medium-size-33 md-small-size-100"></div>
       <div class="md-layout-item md-medium-size-33 md-small-size-100"></div>
     </div>
   </div>
@@ -91,8 +77,7 @@ export default {
       nemCrevette: require("@/assets/img/examples/studio-1.jpg"),
       nemCrabe: require("@/assets/img/examples/studio-1.jpg"),
       nemPoulet: require("@/assets/img/examples/studio-1.jpg"),
-      nemPorc: require("@/assets/img/examples/studio-1.jpg"),
-      brochettesPoulet: require("@/assets/img/examples/studio-1.jpg")
+      nemPorc: require("@/assets/img/examples/studio-1.jpg")
     }
   }
 }
--- src/views/components/PlatsChauds.vue
+++ src/views/components/PlatsChauds.vue
@@ -94,6 +94,25 @@
         </md-card>
       </div>
     </div>
+    <div class="md-layout">
+      <div class="md-layout-item md-medium-size-33 md-small-size-100">
+        <md-card class="md-card-plain">
+          <div class="md-layout-item md-size-50 mx-auto">
+            <img :src="brochettesPoulet" alt="Thumbnail Image" class="img-raised rounded-circle img-fluid">
+          </div>
+          <h4 class="card-title">Brochettes de poulet
+            <br>
+            <small class="card-description text-muted">Poulet</small>
+          </h4>
+
+          <md-card-content>
+            <p class="card-description">You can write here details about one of your team members. You can give more details about what they do. Feel free to add some <a href="#">links</a> for people to be able to follow them outside the site.</p>
+          </md-card-content>
+        </md-card>
+      </div>
+      <div class="md-layout-item md-medium-size-33 md-small-size-100"></div>
+      <div class="md-layout-item md-medium-size-33 md-small-size-100"></div>
+    </div>
   </div>
 </template>
 
@@ -107,7 +126,8 @@ export default {
       legumesSautes: require("@/assets/img/examples/studio-1.jpg"),
       rizCantonais: require("@/assets/img/examples/studio-1.jpg"),
       porcCaramel: require("@/assets/img/examples/studio-1.jpg"),
-      crevettesPiquantes: require("@/assets/img/examples/studio-1.jpg")
+      crevettesPiquantes: require("@/assets/img/examples/studio-1.jpg"),
+      brochettesPoulet: require("@/assets/img/examples/studio-1.jpg")
     }
   }
 }
