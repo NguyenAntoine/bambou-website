6edc62327e7594918d2c5ef7ca85f7610051a130
Antoine <antoine.nguyen3@epsi.fr>
2018-11-13 23:15:29 +0100

    Update docker run script

--- docker-run.sh
+++ docker-run.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
-docker build -t bambou-apache2 .
 docker rm -f bambou-website
-docker run -dit --name bambou-website -p 80:80 --restart=always bambou-apache2
+docker rmi bambou-apache2
+docker build -t bambou-apache2 .
+docker run -dit --name bambou-website -p 80:80 -p 443:443 --restart=always bambou-apache2
