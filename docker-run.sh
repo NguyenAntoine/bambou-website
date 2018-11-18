e477383b0163b7b337b75b35ab9171b29d7c4980
Antoine <antoine.nguyen3@epsi.fr>
2018-11-18 02:53:08 +0100

    Update sh for nom de domaine

--- docker-run.sh
+++ docker-run.sh
@@ -4,8 +4,8 @@ docker rmi bambou-apache2
 docker build -t bambou-apache2 .
 docker run -d \
   --name bambou-website \
-  --env "VIRTUAL_HOST=localhost" \
-  --env "LETSENCRYPT_HOST=localhost" \
+  --env "VIRTUAL_HOST=ns388377.ip-176-31-253.eu" \
+  --env "LETSENCRYPT_HOST=ns388377.ip-176-31-253.eu" \
   --env "LETSENCRYPT_EMAIL=antoine.ngu@outlook.fr" \
   --restart=always \
   bambou-apache2
