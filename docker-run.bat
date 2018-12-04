a844d2d0c79ad78686f6921d87c60663478bb2cc
Antoine <antoine.nguyen3@epsi.fr>
2018-12-04 11:05:42 +0100

    Update script docker

--- docker-run.bat
+++ docker-run.bat
@@ -1,10 +1,4 @@
-docker rm -f bambou-website
+docker-compose down
 docker rmi bambou-apache2
 docker build -t bambou-apache2 .
-docker run -d \
-  --name bambou-website \
-  --env "VIRTUAL_HOST=ns388377.ip-176-31-253.eu" \
-  --env "LETSENCRYPT_HOST=ns388377.ip-176-31-253.eu" \
-  --env "LETSENCRYPT_EMAIL=antoine.ngu@outlook.fr" \
-  --restart=always \
-  bambou-apache2
+docker-compose up -d
